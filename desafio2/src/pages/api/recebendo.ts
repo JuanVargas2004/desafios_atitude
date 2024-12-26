91294e05050d243bce9bfd462f2f50dbaf0f2242
Juan Vargas <juansvc2004@gmail.com>
2024-12-26 12:55:08 -0300

    feat: integrando formulário, api e prisma

--- desafio2/src/pages/api/recebendo.ts
+++ desafio2/src/pages/api/recebendo.ts
@@ -1,4 +1,5 @@
 import { NextApiResponse, NextApiRequest } from "next";
+import adicionar from "@/functions/enviarform";
 
 interface ResponseData {
     message: string
@@ -11,14 +12,18 @@ export default function handler (req: NextApiRequest, res: NextApiResponse<Respo
     const {nome} = req.body
     const {email} = req.body
 
+
     if (req.method === "POST"){
 
-        if (nome){
+        if (nome && email){
             res.status(200).json({
                 message: "Nome recebido com sucesso!",
                 name: String(nome),
                 email: String(email)
             })
+
+            adicionar(nome, email)
+
             console.log(`\n\n\t Nome recebido com sucesso: ${nome} \t\n\n`)
             console.log(`\n\n\t Email recebido com sucesso: ${email} \t\n\n`)
         
