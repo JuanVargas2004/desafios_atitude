e6c14f1b03484d1cf7249f9cc2f8d42e9e6eb650
Juan Vargas <juansvc2004@gmail.com>
2024-12-30 21:33:49 -0300

    feat: alterando a função para adicionar campos de dados pessoais

--- desafio2/src/functions/send_user_db.ts
+++ desafio2/src/functions/send_user_db.ts
@@ -2,13 +2,27 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function send_user(name: string, email: string, password: string){
+export default async function send_user(name: string, email: string, cpf: string, password: string){
     
     await prisma.user_credentials.create({
         data:{
             name: name,
             email: email,
+            cpf: cpf,
             password: password
+        }
+    })
+
+    await prisma.personal_data.create({
+        data:{
+            name: name,
+            dateOfBirth: new Date('2024-01-01'),
+            gender: 'MALE',
+            cpf: '',
+            phone: '',
+            email: email,
+            nationality: '',
+            maritalStatus: 'SINGLE'
         }
     })
 }
