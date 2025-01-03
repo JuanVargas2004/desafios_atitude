7dc2dca789561059b9d0d15fb89326253dbb38cb
Juan Vargas <juansvc2004@gmail.com>
2025-01-03 18:12:09 -0300

    feat: integra formulário com o banco de dados

--- desafio2/src/components/forms/personal.tsx
+++ desafio2/src/components/forms/personal.tsx
@@ -3,11 +3,34 @@
 import { IconArrowNarrowLeft } from '@tabler/icons-react';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
+import jwt from 'jsonwebtoken';
 
 export default function Personal(){
 
     const [isMounted, setIsMounted] = useState(false)
     const router = useRouter()
+
+
+    const token = Cookies.get('token')
+    if (token){
+        const decode = jwt.decode(token)
+        if (decode && typeof decode === 'object'){
+            var id = decode.id
+        }
+    }
+
+
+    const [name, setName] = useState('')
+    const [date, setDate] = useState('')
+    const [gender, setGender] = useState('MALE')
+    const [cpf, setCpf] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [nationality, setNationality] = useState('')
+    const [maritalStatus, setMaritalStatus] = useState('SINGLE')
+
+    const payload = {id, name, date, gender, cpf, phone, email, nationality, maritalStatus}
 
     useEffect( () => {
         setIsMounted(true)
@@ -17,35 +40,60 @@ export default function Personal(){
         return null
     }
 
+
+    const handleSubmit = async (event: React.FormEvent) => {
+
+        event.preventDefault()
+
+        const response = await fetch('/api/personal', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+
+        if (response.ok){
+            alert('Dados Atualizados com Sucesso')
+        } else if (response.status == 500) {
+            alert('Houve um erro ao atualizar os dados')
+        } else {
+            alert('Um erro inesperado aconteceu')
+        }
+
+    }
+
+
     return (
         <div className='h-screen flex flex-col justify-center items-center'>
             
-            <div className='w-full ml-[700px]' onClick={() => {router.back()}}>
+            <div className='w-full ml-[700px]'>
                 <IconArrowNarrowLeft
                     size={65}
                     className='transition ease hover:scale-110 active:text-[#7a7a7a] cursor-pointer'
+                    onClick={() => {router.push('/welcome/')}}
                 />
             </div>
 
 
-            <form>
+            <form onSubmit={handleSubmit} method="POST">
 
                 <div className="flex flex-col gap-3">
 
 
                     <div className="container_input_form">
                         <label htmlFor="name">Nome Completo:</label>
-                        <input type="text" id="name" className="input_form" placeholder="Digite aqui seu Nome"/>
+                        <input type="text" id="name" className="input_form" placeholder="Digite aqui seu Nome" value={name} onChange={(e) => setName(e.target.value)}/>
                     </div>
 
                     <div className="container_input_form">
                         <label htmlFor="dateOfBirth">Data de Nascimento:</label>
-                        <input type="date" id="dateOfBirth" className="input_form"/>
+                        <input type="date" id="dateOfBirth" className="input_form" value={date} onChange={(e) => setDate(e.target.value)}/>
                     </div>
 
                     <div className="container_input_form">
                         <label htmlFor="gender">Gênero:</label>
-                        <select id="gender" className="input_form">
+                        <select id="gender" className="input_form" value={gender} onChange={(e) => setGender(e.target.value)}>
                             <option value="MALE">Masculino</option>
                             <option value="FEMALE">Feminino</option>
                             <option value="OTHER">Outro</option>
@@ -55,27 +103,27 @@ export default function Personal(){
 
                     <div className="container_input_form">
                         <label htmlFor="cpf">CPF:</label>
-                        <input type="text" id="cpf" className="input_form" placeholder="Digite seu CPF"/>
+                        <input type="text" id="cpf" className="input_form" placeholder="Digite seu CPF" value={cpf} onChange={(e) => setCpf(e.target.value)}/>
                     </div>
 
                     <div className="container_input_form">
                         <label htmlFor="phone">Telefone:</label>
-                        <input type="text" id="phone" className="input_form" placeholder="Digite seu Telefone"/>
+                        <input type="text" id="phone" className="input_form" placeholder="Digite seu Telefone" value={phone} onChange={(e) => setPhone(e.target.value)}/>
                     </div>
                     
                     <div className="container_input_form">
                         <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" className="input_form" placeholder="Digite seu Email"/>
+                        <input type="email" id="email" className="input_form" placeholder="Digite seu Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     </div>
 
                     <div className="container_input_form">
                         <label htmlFor="nationality">Nacionalidade:</label>
-                        <input type="text" id="nationality" className="input_form" placeholder="Digite sua Nacionalidade"/>
+                        <input type="text" id="nationality" className="input_form" placeholder="Digite sua Nacionalidade" value={nationality} onChange={(e) => setNationality(e.target.value)}/>
                     </div>
 
                     <div className="container_input_form">
                         <label htmlFor="maritalStatus">Estado Civil:</label>
-                        <select id="maritalStatus" className="input_form">
+                        <select id="maritalStatus" className="input_form" value={maritalStatus} onChange={(e) => setMaritalStatus(e.target.value)}>
                             <option value="SINGLE">Solteiro(a)</option>
                             <option value="MARRIED">Casado(a)</option>
                             <option value="DIVORCED">Divorciado(a)</option>
